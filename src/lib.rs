5fcea25a5259662e1464e8bfbebc7118c09141d7
frost-head <ed.ayush2003@gmail.com>
2024-05-10 13:30:12 +0530

    implimented faculty

--- src/lib.rs
+++ src/lib.rs
@@ -1,62 +1,77 @@
-// implimentation of Student 
+// implimentation of Student
 
-enum Stream{
-    General, 
-    PCM,
-    PCB, 
-    Both
+pub mod falulty {
+    pub enum Qualification {
+        BEd,
+        BSc,
+        BTech,
+        BArt,
+        MArt,
+        MSc,
+        MTech,
+        Other,
+        BBa,
+        MBa,
+    }
+
+    pub enum Position {
+        Principle,
+        VPrinnciple,
+        Accountance,
+        Teacher,
+        Clerk,
+    }
+    pub struct Faculty {
+        name: String,
+        age: u8,
+        f_id: u32,
+        qualification: Qualification,
+        position: Position,
+        can_teach: u8,
+    }
+
+    impl Faculty{
+        pub fn new(name:String,age: u8, f_id:u32, qualification: Qualification , positon: Position, can_teach:u8) -> Faculty {
+            Faculty{
+                name: name, 
+                age : age, 
+                f_id: f_id,
+                qualification: qualification,
+                position: positon, 
+                can_teach: can_teach
+            }
+        }
+        pub fn info(self) {
+            println!("{}", self.name);
+        }
+    }
+}
+pub mod student {
+    pub enum Stream {
+        General,
+        PCM,
+        PCB,
+        Both,
+    }
+    pub struct Student {
+        name: String,   // Name of the Student
+        age: u8,        // age of the Studne
+        standard: u8,   // Standard of the student
+        stream: Stream, // Stream of the student only avilable if the standard of the student is >10
+        roll_no: u32,   // Roll number of the student must be uniquely identifable.
+    }
 }
 
-enum Qualification{
-    BEd, 
-    BSc, 
-    BTech, 
-    BArt,
-    MArt, 
-    MSc, 
-    MTech,
-    Other, 
-    BBa, 
-    MBa, 
-}
-
-enum Position{
-    Principle, 
-    VPrinnciple, 
-    Accountance, 
-    Teacher,
-    Clerk, 
-}
-
-
-enum Affiliation{
-    CBSE, 
-    HPBOSE
-}
-
-struct Student{
-    name : String,  // Name of the Student  
-    age : u8,  // age of the Studne 
-    standard : u8,  // Standard of the student
-    stream : Stream, // Stream of the student only avilable if the standard of the student is >10 
-    roll_no : u32,  // Roll number of the student must be uniquely identifable.
-}
-
-
-struct Faculty{
-    name: String, 
-    age : u8, 
-    f_id: u32, 
-    qualification : Qualification, 
-    position : Position, 
-    can_teach : u8,
-}
-
-
-struct School{
-    name : String, 
-    s_id : u8, 
-    affiliation : Affiliation, 
-    principle : Faculty, 
-    // TODO
+pub mod school {
+    pub enum Affiliation {
+        CBSE,
+        HPBOSE,
+    }
+    pub struct School {
+        name: String,
+        s_id: u8,
+        affiliation: Affiliation,
+        principle: crate::falulty::Faculty,
+        // TODO
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 
 
-
-
-
+fn main(){
+    let a = school_managment::falulty::Faculty::new(String::from("ayush"), 8, 0, school_managment::falulty::Qualification::BArt, school_managment::falulty::Position::Accountance, 12);
+    println!("Just a place holder!");
+    a.info();
+}
 
 
 
